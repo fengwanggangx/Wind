9033f70759a971dad71e24a9fb5c05007628244b
fengwanggangx <457081870@qq.com>
2026-09-13 21:43:14 +0800

    style 代码优化

--- Wind/database/CMySQL.cpp
+++ Wind/database/CMySQL.cpp
@@ -26,12 +26,17 @@ namespace db
 		if (nullptr == m_pDB)
 		{
 			m_pDB = mysql_init(nullptr);
-			if (m_pDB == nullptr) return 1;
+			if (nullptr == m_pDB)
+			{
+				return 1;
+			}
 		}
 
 		mysql_options(m_pDB, MYSQL_SET_CHARSET_NAME, param.m_strCharset.c_str());
-		if (nullptr == mysql_real_connect(m_pDB, param.m_strHost.c_str(), param.m_strAccount.c_str(), param.m_strPasswd.c_str(),
-								param.m_strDataBase.c_str(), param.m_nPort, nullptr, 0))
+
+		bool bReconnect = true;
+		mysql_options(m_pDB, MYSQL_OPT_RECONNECT, &bReconnect);
+		if (nullptr == mysql_real_connect(m_pDB, param.m_strHost.c_str(), param.m_strAccount.c_str(), param.m_strPasswd.c_str(), param.m_strDataBase.c_str(), param.m_nPort, nullptr, 0))
 		{
 			std::cerr << "MySQL connection failed: " << mysql_error(m_pDB) << '\n';
 			mysql_close(m_pDB);
